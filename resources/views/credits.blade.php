acc6d1694fe376b5049bd18e519fc25fdcb064a4
ArellanoF <CORREO@example.comfrancescarellano@gmail.com>
2022-06-06 11:09:57 +0200

    initial commit

---
+++ resources/views/credits.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="panel panel-primary">
+    <div class="panel-heading">Sistema de gestión de incidencias</div>
+    <div class="rowcredit" style="padding:2rem;">
+        <div class="credit2" style="background: #DADCD5;
+    border-radius: 1rem;">
+            <div class="panel-body text-center">
+                <h1>Proyecto realizado con Laravel</h1>
+            </div>
+            <div class="panel-body">
+                <p>Siguiendo los tutoriales de: <a href="https://www.youtube.com/c/SorcJC" target="_blank">Programación y más</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
